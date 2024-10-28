ed5576af003a1bd930a18c4d87a660f64282667e
mercy vincent <mercydanke@gmail.com>
2024-10-28 02:24:33 +0100

    update workflow

--- src/module/v1/waitList/waitList.module.ts
+++ src/module/v1/waitList/waitList.module.ts
@@ -4,6 +4,7 @@ import { WaitListController } from './waitList.controller';
 import { WaitList, WaitListSchema } from './schema/waitList.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RepositoryModule } from '../repository/repository.module';
+import { OtpModule } from '../otp/otp.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { RepositoryModule } from '../repository/repository.module';
       { name: WaitList.name, schema: WaitListSchema },
     ]),
     RepositoryModule,
+    OtpModule,
   ],
   controllers: [WaitListController],
   providers: [WaitListService],
