ed154e1a56e948c118197e6d9faee3fda9991e6a
mercy vincent <mercydanke@gmail.com>
2024-11-14 13:44:59 +0100

    feat(withdrawal): implement user withdrawal request flow

--- src/module/v1/withdrawal/controllers/user-withdrawal.controller.ts
+++ src/module/v1/withdrawal/controllers/user-withdrawal.controller.ts
@@ -5,7 +5,7 @@ import { UserDocument } from '../../user/schemas/user.schema';
 import { CreateWithdrawalDto } from '../dto/withdrawal.dto';
 import { UserWithdrawalService } from '../services/user-withdrawal.service';
 
-@Controller('user-withdrawer')
+@Controller('user/withdrawer')
 export class UserWithdrawalController {
   constructor(private userWithdrawalService: UserWithdrawalService) {}
 
